6c314b9b3839025560d7b2a83fe674d8150a349a
Vinicius <vini383@gmail.com>
2024-04-03 12:09:44 -0300

    add save tour endpoint

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const fs = require('fs');
 
+const toursJson = `${__dirname}/dev-data/data/tours.json`;
+
 const app = express();
 
 app.use(express.json());
 
 const port = 3000;
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
-);
+const tours = JSON.parse(fs.readFileSync(toursJson));
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).send({
@@ -21,7 +21,23 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('/api/v1/tours', (req, res) => {});
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(toursJson, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: 'Success',
+      data: {
+        tour: newTour,
+      },
+    });
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log('Listening');
